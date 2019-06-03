19fed980db4db6eaeeb68636569358d83d954d96
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-03 13:44:49 +0100

    Installed reactstrap and imported it into my project

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from "react"
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from "reactstrap"
 import dummyData from './dummy-data'
 import PostContainer from "./components/PostContainer/PostContainer"
 import SearchBar from "./components/SearchBar/SearchBar"
@@ -10,22 +12,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-      
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <SearchBar />
         <PostContainer />
+        <Button color="primary">primary</Button>
       </div>
     );
     
